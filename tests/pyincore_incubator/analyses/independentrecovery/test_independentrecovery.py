be5f646551034207127647e97cb6b87703f30544
Chris Navarro <cmnavarr@illinois.edu>
2025-05-15 09:15:19 -0500

    Added sample data to IN-CORE service and replaced test data with IDs

--- tests/pyincore_incubator/analyses/independentrecovery/test_independentrecovery.py
+++ tests/pyincore_incubator/analyses/independentrecovery/test_independentrecovery.py
@@ -1,4 +1,4 @@
-from pyincore import IncoreClient, Dataset
+from pyincore import IncoreClient
 from pyincore_incubator.analyses.independentrecovery import IndependentRecovery
 import pyincore.globals as pyglobals
 
@@ -10,38 +10,31 @@ def run_with_base_class():
 
     independent_recovery = IndependentRecovery(client)
 
-    building_damage = Dataset.from_file(
-        "data/Joplin_bldg_dmg_result.csv", "ergo:buildingDamageVer4"
-    )
-    household_inventory = Dataset.from_file(
-        "data/hui_ver2-0-0_Joplin_MO_2010_rs1000.csv", "incore:housingUnitInventory"
-    )
-    household_allocation = Dataset.from_file(
-        "data/Joplin_hua_result_1238.csv", "incore:housingUnitAllocation"
-    )
-    household_dislocation = Dataset.from_file(
-        "data/pop-dislocation-results.csv", "incore:popDislocation"
-    )
-    residential_recovery = Dataset.from_file(
-        "data/joplin_recovery_sample10_recovery.csv", "incore:buildingRecovery"
-    )
-    housing_recovery = Dataset.from_file(
-        "data/housing_recovery_result.csv", "incore:housingRecoveryHistory"
+    building_dmg_id = "6824dff3ce15ce6bdf893282"
+    housing_inventory_id = "6824e19fce15ce6bdf89328a"
+    housing_unit_id = "6824e277ce15ce6bdf893293"
+    population_dislocation_id = "6824e49ace15ce6bdf8932a5"
+    residential_recovery_id = "6824e4c1ce15ce6bdf8932a6"
+    housing_recovery_id = "6824e4fdce15ce6bdf8932a7"
+
+    independent_recovery.load_remote_input_dataset("building_damage", building_dmg_id)
+    independent_recovery.load_remote_input_dataset(
+        "housing_unit_inventory", housing_inventory_id
     )
 
-    independent_recovery.set_input_dataset("building_damage", building_damage)
-    independent_recovery.set_input_dataset(
-        "housing_unit_inventory", household_inventory
+    independent_recovery.load_remote_input_dataset(
+        "housing_unit_allocation", housing_unit_id
     )
-    independent_recovery.set_input_dataset(
-        "housing_unit_allocation", household_allocation
+
+    independent_recovery.load_remote_input_dataset(
+        "population_dislocation", population_dislocation_id
     )
-    independent_recovery.set_input_dataset(
-        "population_dislocation", household_dislocation
+
+    independent_recovery.load_remote_input_dataset(
+        "residential_recovery", residential_recovery_id
     )
-    independent_recovery.set_input_dataset("residential_recovery", residential_recovery)
-    independent_recovery.set_input_dataset(
-        "household_housing_recovery", housing_recovery
+    independent_recovery.load_remote_input_dataset(
+        "household_housing_recovery", housing_recovery_id
     )
 
     # Specify the result name
